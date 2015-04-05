170080516c64b6a160307b8dc85606bb2082f3ad
Nobuhito Ibaraki <niba1122@gmail.com>
2015-04-05 15:30:29 +0900

    aboutusを復活。消去していたTopへのルーティングを復活。

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 React.render(
 React.createElement(Router, {pushState: true, root: ""}, 
+	React.createElement(Content, {path: "", reactClass: "Top"}), 
+	React.createElement(Content, {path: "aboutus", reactClass: "AboutUs"}), 
 	React.createElement(Content, {path: "concerts", reactClass: "Concerts"}), 
 	React.createElement(Content, {path: "tickets", reactClass: "Tickets"}), 
 	React.createElement(Content, {path: "contactus", reactClass: "ContactUs"})
