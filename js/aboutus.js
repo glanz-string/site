e66ba61a2384b1932555720c2473d0805054bb68
Nobuhito Ibaraki <niba1122@gmail.com>
2015-04-05 15:48:00 +0900

    aboutusのレイアウト調整

--- js/aboutus.js
+++ js/aboutus.js
@@ -12,7 +12,7 @@ React.createElement("section", {className: "content mt-30px"},
       React.createElement("img", {src: "images/playing.png"})
     ), 
     React.createElement("div", {className: "column2-right"}, 
-      React.createElement("p", {className: "text"}, "阪本正彦氏を中心にプロ奏者からアマチュア奏者、学生など様々なメンバーが集まって創立された弦楽合奏団です。", React.createElement("br", null), 
+      React.createElement("p", {className: "text ml-10px"}, "阪本正彦氏を中心にプロ奏者からアマチュア奏者、学生など様々なメンバーが集まって創立された弦楽合奏団です。", React.createElement("br", null), 
 "既存の弦楽合奏の形にとらわれず、聴きにきてくださる方が再度聴きたくなるような独自性のある演奏会を目指しています。熱い演奏を、そして楽しい演奏会を、是非聴きにいらして下さい。"
       )
     )
